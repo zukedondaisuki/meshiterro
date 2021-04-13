4cd8a983b095a4cd46a589ee52153e93193e2ea1
zukedondaisuki <a.happy.coaster@outlook.jp>
2021-04-13 12:14:29 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a4b41bbee781c7df057c1b2c58b5afd2c5def924f0651bd580ae1ca36c8f73491f08b373db47fbf0226ae57c36f83c417068ea0e7ca7474a2762983a21b5d493'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_031017) do
+ActiveRecord::Schema.define(version: 2021_04_13_085949) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.string "image_id"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
