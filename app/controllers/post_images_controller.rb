e7c72d708bb8ee31886cf998146db67fad8c0faa
zukedondaisuki <a.happy.coaster@outlook.jp>
2021-04-14 09:33:12 +0000

    [Finish]4-6

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,7 +8,11 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -26,7 +30,7 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
- 
+
  private
 
   def post_image_params
